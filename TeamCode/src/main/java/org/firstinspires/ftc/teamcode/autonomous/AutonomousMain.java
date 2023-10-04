39dacffce66572606610b25ce27110e8fedb3327
FreehandBlock51 <60456293+FreehandBlock51@users.noreply.github.com>
2023-10-04 13:58:36 -0700

    Update AutonomousMain.java
    nothing to see here

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousMain.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.teamcode.OpBase;
 
-@Autonomous(name = "Motor Melter")
+@Autonomous(name = "Autonomous Control")
 public class AutonomousMain extends OpBase {
 
     @Override
